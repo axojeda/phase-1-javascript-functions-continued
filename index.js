8a83266cc4b109b3e0b874e083a970971183c737
Aura <ax.pink@gmail.com>
2022-12-27 11:18:41 -0500

    commit tree 174dbfd466df2356c0a1fae4e2acfa1cc72fb1ee

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+function saturdayFun (fun = "roller-skate") {
+    return `This Saturday, I want to ${fun}!`
+}
+
+const mondayWork = function(work = "go to the office") {
+    return `This Monday, I will ${work}.`
+}
+
+function wrapAdjetive(adjetive = "special") {
+    return `You are *${adjetive}*`
+}
+console.log(wrapAdjetive)
